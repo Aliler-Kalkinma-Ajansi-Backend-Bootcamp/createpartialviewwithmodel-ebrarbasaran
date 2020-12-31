6183deaabb625ae695975fe2f173bfb4df9abd9f
ebrarbasaran <76623730+ebrarbasaran@users.noreply.github.com>
2020-12-31 14:12:19 +0300

    Add project files.

---
+++ ebOdev2/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ebOdev2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult FilmBilgi(Models.Bilgiler data)
+        {
+            var bilgi = new Models.Bilgiler();
+            bilgi.FilmAdi = "Tenet";
+            bilgi.Yonetmen = "Christopher Nolan ";
+            bilgi.Turu = "Aksiyon,Bilim Kurgu";
+            bilgi.ImdbPuani = 7.6;
+
+            return View(bilgi);
+        }
+    }
+}
---
+++ ebOdev2/Models/Bilgiler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ebOdev2.Models
+{
+    public class Bilgiler
+    {
+        public string FilmAdi { get; set; }
+        public string Yonetmen { get; set; }
+        public string Turu { get; set; }
+        public double ImdbPuani { get; set; }
+    }
+}
